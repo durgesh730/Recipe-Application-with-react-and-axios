313b4e061912e3938543d4a6094f41b5dd95033d
durgesh730 <durgesh.unicoglobal@gmail.com>
2024-04-07 17:19:06 +0530

    Not Found

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import "./HomePage.scss";
 import { useMealContext } from '../../context/mealContext';
 import MealList from "../../components/Meal/MealList";
+import NotFound from '../../components/NotFound/NotFound';
 
 const HomePage = () => {
   const { meals, categoryMeals } = useMealContext();
   return (
     <main className='main-content'>
       {
-        !meals ? <div style={{ textAlign: "center", paddingTop: "50px" }} >Meal Not Found</div> : meals.length > 0 ?
+        !meals ? <div style={{ textAlign: "center", paddingTop: "50px" }} > <NotFound />  </div> : meals.length > 0 ?
           <MealList meals={meals} /> :
           <MealList meals={categoryMeals} />
       }
